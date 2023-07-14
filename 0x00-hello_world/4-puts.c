81b832a7b21ffcfd4234c6894a2155e841c235e1
ekenesamuel8 <chikwenduekene8@gmail.com>
2023-07-13 17:16:10 -0700

    tadsk 234

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+# include <stdio.h>
+\*
+*main will print a word
+*it will reture 0
+*\
+int main()
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return 0;
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+this is my ever first experince on programming in alx school
+and is startin with C programming procedual
