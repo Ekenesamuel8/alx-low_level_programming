c31da966dac5b02472d8edaf8aa8564120baee45
ekenesamuel8 <chikwenduekene8@gmail.com>
2023-07-24 03:31:17 +0000

    corrections made recently

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -35,9 +35,11 @@ void print_times_table(int n)
 					_putchar((k % 10) + '0');
 				} else if (k >= 100)
 				{
-					_putchar(',')																				_putchar(' ');
-					_putchar((k / 100) + '0')
-					_putchar(((k / 10) % 10) + '0'																		_Putchar((k % 10) + '0');
+					_putchar(',');
+					_putchar(' ');
+					_putchar((k / 100) + '0');
+					_putchar(((k / 10) % 10) + '0');
+					_Putchar((k % 10) + '0');
 				}
 			}
 			_putchar('\n');
