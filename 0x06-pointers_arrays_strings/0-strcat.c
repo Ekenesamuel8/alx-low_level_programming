4d7234619b8e9e3119a95b842bdaf0688d13b52b
ekenesamuel8 <chikwenduekene8@gmail.com>
2023-07-28 18:43:44 +0000

    task 0

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ *_strcat - concentacate two strings
+ *@dest: strings 1
+ *@src: strings 2
+ *Return: dest
+ */
+
+char *_strcat(char *dest, char *src)
+{
+	int q, slen, dlen;
+
+	slen = 0;
+	dlen = 0;
+	for (q = 0; dest[q] != '\0'; q++)
+		dlen++;
+	for (q = 0; src[q] != '\0'; q++)
+		slen++;
+	for (q = 0; q <= slen; q++)
+	{
+		dest[slen + q] = src[q];
+	}
+	dest[q] = '\0';
+	return (dest);
+}
