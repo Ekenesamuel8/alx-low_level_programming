46f5bd0a04e2e0234fe5a25e90a96378a127b56a
ekenesamuel8 <chikwenduekene8@gmail.com>
2023-08-09 20:54:27 +0000

    completed tasks

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -3,8 +3,8 @@
 
 /**
  * str_concat - get ends of input and add together for size
- * @s1: input one to concat 		 		 		
- * @s2: input two to concat	
+ * @s1: input one to concat
+ * @s2: input two to concat
  * Return: concat of s1 and s2
  */
 char *str_concat(char *s1, char *s2)
@@ -25,12 +25,12 @@ char *str_concat(char *s1, char *s2)
 		ci++;
 
 	conct = malloc(sizeof(char) * (i + ci + 1));
-	
+
 	if (conct == NULL)
 		return (NULL);
-	
+
 	i = ci = 0;
-	
+
 	while (s1[i] != '\0')
 	{
 		conct[i] = s1[i];
