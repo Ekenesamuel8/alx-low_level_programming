dc05096d4f5e5e245b9ca1d9da33edb79e58e6b6
ekenesamuel8 <chikwenduekene8@gmail.com>
2023-08-15 17:44:52 +0000

    corrections

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -17,7 +17,7 @@ struct dog
 /**
  * dog_t - typedef for struct dog
  */
-typedef struct dog dog_t
+//typedef struct dog dog_t
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
