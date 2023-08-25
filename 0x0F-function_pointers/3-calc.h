16743042fc844e42a337871515b32f89331470b5
ekenesamuel8 <chikwenduekene8@gmail.com>
2023-08-25 23:05:17 +0000

    some corrections made

--- 0x0F-function_pointers/3-calc.h
+++ 0x0F-function_pointers/3-calc.h
@@ -1,42 +1,22 @@
-#include "function_pointers.h"
-#include <stdlib.h>
-#include <stdio.h>
-#include "3-calc.h"
+#ifndef CALC_H
+#define CALC_H
 /**
- * main - Prints the result of simple operations.
- * @argc: The number of arguments supplied to the program.
- * @argv: An array of pointers to the arguments.
- *
- * Return: Always 0.
+ * struct op - A struct op.
+ * @op: The operator.
+ * @f: The associated function.
  */
-int main(int __attribute__((__unused__)) argc, char *argv[])
+typedef struct op
 {
-	int num1, num2;
-	char *op;
+		char *op;
+			int (*f)(int a, int b);
+} op_t;
 
-	if (argc != 4)
-	{
-		printf("Error\n");
-		exit(98);
-	}
+int op_add(int a, int b);
+int op_sub(int a, int b);
+int op_mul(int a, int b);
+int op_div(int a, int b);
+int op_mod(int a, int b);
+int (*get_op_func(char *s))(int, int);
 
-	num1 = atoi(argv[1]);
-	op = argv[2];
-	num2 = atoi(argv[3]);
+#endif
 
-	if (get_op_func(op) == NULL || op[1] != '\0')
-	{
-		printf("Error\n");
-		exit(99);
-	}
-
-	if ((*op == '/' && num2 == 0) ||
-			(*op == '%' && num2 == 0))
-	{
-		printf("Error\n");
-		exit(100);
-	}
-
-	printf("%d\n", get_op_func(op)(num1, num2));
-	return (0);
-}
