0c0bc50029e77970f83f694b33b530453c93db25
ekenesamuel8 <chikwenduekene8@gmail.com>
2023-08-25 21:00:32 +0000

    all task successfull

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -7,6 +7,7 @@
 size_t print_list(const list_t *h)
 {
 	size_t nuud;
+
 	nuud = 0;
 
 	while (h != NULL)
--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -5,5 +5,6 @@ void showfirst(void)__attribute__ ((constructor));
  */
 void showfirst(void)
 {
-	printf("You're beat! and yet, you must allow,\nI bore my house upon my back!\n");
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
 }
