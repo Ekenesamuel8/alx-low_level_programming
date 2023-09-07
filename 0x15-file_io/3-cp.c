b33e7dad80f67084e0b9928f5b1ee0711f6483bc
ekenesamuel8 <chikwenduekene8@gmail.com>
2023-09-07 08:59:04 +0000

    all

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -1,36 +1,56 @@
 #include "main.h"
 
-void errorprint(int num, char *string, char *arg)
+void errorprint(int opento, int openfrom, char *argv[])
 {
-	dprintf(STDOUT_FILENO, string, arg);
-	exit(num);
+	if (opento == -1)
+	{
+		dprintf(STDOUT_FILENO, "Error: can't write to %s\n", argv[2]);
+		exit(99);
+	}
+	if (openfrom == -1)
+	{
+		dprintf(STDOUT_FILENO, "Error: cant't read from file %s\n", argv[1]);
+		exit(98);
+	}
 }
 
-int main(int argc, char argv[])
+int main(int argc, char *argv[])
 {
-	if (argc != 3)
-		errorprint(97, "Usage: cp file_from file_to\n", argv);
-	char *cp_from;
-	char *cp_to;
-	int opento, openfrom, run;
+	int opento, openfrom, closefile;
 	ssize_t red, ite;
-	char *buff;
-
-	opento = open(argv[2], O_CREAT | O_WRONLY | O_APPEND, 0664);
-	if (opento = -1)
-		errorprint(98, "Can't write to NAME_OF_THE_FILE\n", argv[2]);
+	char buff[1024];
+	if (argc != 3)
+	{
+		dprintf(STDOUT_FILENO, "%s\n", "Usage: cp file_from file_to");
+		exit(97);
+	}
 
 	openfrom = open(argv[1], O_RDONLY);
-	if (openfrom = -1)
-		errorprint(99, "Error: Can't read from file NAME_OF_THE_FILE\n", argv[1]);
-	for (run = 0; run[red = read(openfrom, buff, 1024)]; run++)
+	opento = open(argv[2], O_CREAT | O_WRONLY | O_APPEND | O_TRUNC, 0664);
+	errorprint(opento, openfrom, argv);
+	red = 1024;
+	while (red == 1024)
 	{
-		ite = write(opento, buff, run);
-		if (ite = -1)
-			return (-1);
+		red = read(openfrom, buff, red);
+		if (red == -1)
+			errorprint(0, -1, argv);
+		ite = write(opento, buff, red);
+		if (ite == -1)
+			errorprint(-1, 0, argv);
 	}
-	if (red = -1)
-		return (-1);
+	closefile = close(openfrom);
+	if (closefile == -1)
+	{
+		dprintf(STDOUT_FILENO, "Error: can't close fd %d\n", openfrom);
+		exit(100);
+	}
+	closefile = close(opento);
+	if (closefile == -1)
+	{
+		dprintf(STDOUT_FILENO, "Error: can't close fd %d\n", opento);
+		exit(100);
+	}
+	return (0);
 
 	close(opento);
 	close(openfrom);
