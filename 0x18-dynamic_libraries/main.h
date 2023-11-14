cee6d926505d4f16f865b803fd10cdf60a5b28f3
ekene samuel <chikwenduekene@gmail.com>
2023-11-14 01:38:17 +0000

    python dynamics

--- 0x18-dynamic_libraries/main.h
+++ 0x18-dynamic_libraries/main.h
@@ -1,5 +1,6 @@
 #ifndef _MAIN_H_
 #define _MAIN_H_
+
 int _putchar(char c);
 int _islower(int c);
 int _isalpha(int c);
---
+++ 0x18-dynamic_libraries/operaation.c
@@ -0,0 +1,62 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+* add - add two numbers
+* @s: first number
+* @d: second number
+* Return: addition
+*/
+
+int add(int s, int d)
+{
+	return(s + d);
+}
+
+/**
+* sub - subtract two numbers
+* @s: first number
+* @d: second number
+* Return: subtraction
+*/
+
+int sub(int s, int d)
+{
+	return(s - d);
+}
+
+/**
+* mul - multiple two numbers
+* @s: first number
+* @d: second number
+* Return: multiplication
+*/
+
+int mul(int s, int d)
+{
+	return(s * d);
+}
+
+/**
+* divd - divide two numbers
+* @s: first number
+* @d: second number
+* Return: divition
+*/
+
+int divd(int s, int d)
+{
+	return(s / d);
+}
+
+/**
+* mod - modulate two numbers
+* @s: first number
+* @d: second number
+* Return: modulo
+*/
+
+int mod(int s, int d)
+{
+	return(s % d);
+}
